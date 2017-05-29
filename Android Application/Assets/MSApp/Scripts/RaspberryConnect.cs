dde867bdf29e474a9edff5e9813ecd0730510b3c
Elvis Caramete <elvis.caramete@gmail.com>
2017-05-29 10:20:32 +0300

    Update server and mobile application

--- Android Application/Assets/MSApp/Scripts/RaspberryConnect.cs
+++ Android Application/Assets/MSApp/Scripts/RaspberryConnect.cs
@@ -18,23 +18,42 @@ public class RaspberryConnect : MonoBehaviour {
 	public NetworkStream theStream;
 	StreamWriter theWriter;
 	StreamReader theReader;
-	public String Host = "10.42.0.111";
+	public String Host = "";
 	public Int32 Port = 50001;
+
+	public Text humidityValue;
+	private int prevPump = 2;
+	float valueHumidity ;
+
+	public Text temperatureValue;
+	private int prevTemp = 2;
+	float valueTemperature ;
+
+	public Text moistureValue;
+	private int prevMoist = 2;
+
+	public Text lightValue;
+	private int prevLight   = 2;
 
 
 	void Start(){
 		myImageComponent.sprite = standBy;
+		humidityValue.text = "Humidity Value: Connect First";
+		temperatureValue.text = "Temperature Value: Connect First";
 	}
 	void Update() {
-		if(socketReady == true)
-			while (theStream.DataAvailable) {                  // if new data is recieved from Arduino
-				string recievedData = readSocket();            // write it to a string
-					
-				if (recievedData == "Ceva") {            // compare that string and adjust the current light status accordingly
-					Debug.Log("Ceva");
-				}
-					
-			}
+
+		if (socketReady == true && (prevPump == 1 || prevPump == 0)) {
+			humidityValue.text = "Humidity value: " + String.Format ("{0:00.00}", valueHumidity).ToString ();
+		} else if (socketReady == true)
+			humidityValue.text = "Temperature Value: None";
+
+		if (socketReady == true && (prevTemp == 1 || prevTemp == 0)) {
+			temperatureValue.text = "Temperature value: "+ String.Format("{0:00.00}", valueTemperature).ToString ();
+		} else if (socketReady == true) 
+			temperatureValue.text = "Temperature Value: None";
+
+
 		if(myImageComponent.sprite == succesDisconnect)
 			StartCoroutine (ExecuteAfterTime (5));
 	}
@@ -77,6 +96,115 @@ public class RaspberryConnect : MonoBehaviour {
 			Debug.Log("Socket error:" + e);                // catch any exceptions
 			myImageComponent.sprite = succesDisconnect;
 		}
+	}
+
+	//HUMIDITY Functions
+	public void humidityTurnON() { 
+		if (!socketReady){
+			Debug.Log ("Connect first");
+			return;
+		}
+		prevPump = 1;
+		valueHumidity = UnityEngine.Random.Range (44f, 54f);
+		String tmpString = "PumpON";
+		theWriter.Write(tmpString);
+		theWriter.Flush();
+	}
+
+	public void humidityTurnOFF() {     
+		if (!socketReady){
+			Debug.Log ("Connect first");
+			return;
+		}
+		if (prevPump == 1) {
+			valueHumidity = UnityEngine.Random.Range (44f, 54f);
+			String tmpString = "PumpOFF";
+			theWriter.Write (tmpString);
+			theWriter.Flush ();
+			prevPump = 0;
+		}
+		Debug.Log ("Turn ON first");
+	}
+
+	//TEMPERATURE Functions
+	public void temperatureTurnON() { 
+		if (!socketReady){
+			Debug.Log ("Connect first");
+			return;
+		}
+		prevTemp = 1;
+		valueTemperature = UnityEngine.Random.Range (24f, 26f);
+		String tmpString = "LedON";
+		theWriter.Write(tmpString);
+		theWriter.Flush();
+	}
+
+	public void temperatureTurnOFF() {     
+		if (!socketReady){
+			Debug.Log ("Connect first");
+			return;
+		}
+		if (prevTemp == 1) {
+			valueTemperature = UnityEngine.Random.Range (24f, 26f);
+			String tmpString = "LedOFF";
+			theWriter.Write (tmpString);
+			theWriter.Flush ();
+			prevTemp = 0;
+		}
+		Debug.Log ("Turn ON first");
+	}
+
+
+	//MOISTURE Functions
+
+	public void moistureTurnON() { 
+		if (!socketReady){
+			Debug.Log ("Connect first");
+			return;
+		}
+		prevMoist = 1;
+		String tmpString = "MoistON";
+		theWriter.Write(tmpString);
+		theWriter.Flush();
+	}
+
+	public void moistureTurnOFF() {     
+		if (!socketReady){
+			Debug.Log ("Connect first");
+			return;
+		}
+		if (prevMoist == 1) {
+			String tmpString = "MoistOFF";
+			theWriter.Write (tmpString);
+			theWriter.Flush ();
+		}
+		Debug.Log ("Turn ON first");
+	}
+
+	//LIGHT Functions
+
+	public void lightTurnON() { 
+		if (!socketReady){
+			Debug.Log ("Connect first");
+			return;
+		}
+		prevLight = 1;
+		String tmpString = "LightON";
+		theWriter.Write(tmpString);
+		theWriter.Flush();
+	}
+
+	public void lightTurnOFF() {     
+		if (!socketReady){
+			Debug.Log ("Connect first");
+			return;
+		}
+		if (prevLight == 1) {
+			String tmpString = "LightOFF";
+			theWriter.Write (tmpString);
+			theWriter.Flush ();
+		}
+		Debug.Log ("Turn ON first");
 	}
 
 	public void writeSocket() {            // function to write data out
--- Raspberry Server/server_socket.py
+++ Raspberry Server/server_socket.py
@@ -1,24 +1,66 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
 import socket
+import sys
+import Adafruit_DHT
+import time
+
 
 backlog=1
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("172.16.254.90",50001))
+s.bind(("168.154.1.1",50001))
 s.listen(backlog)
+
+humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+def callback(channel):
+        if GPIO.input(channel):
+                print "Low soil moisture"
+
+        else:
+                print "Good soil moisture"
+
+
+# Set our GPIO numbering to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Define the GPIO pin that we have our digital output from our sensor connected                                                                                                              to
+channel = 17
+# Set the GPIO pin to an input
+GPIO.setup(channel, GPIO.IN)
+
+# This line tells our script to keep an eye on our gpio pin and let us know when                                                                                                              the pin goes HIGH or LOW
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+# This line asigns a function to the GPIO pin so that when the above line tells                                                                                                              us there is a change on the pin, run this function
+
 try:
     print ( "is waiting")
     client, address = s.accept()
-
+    print "Connected"
     while 1:
+        print humidity
+        print temperature
+        print "Wating for command"
+        GPIO.add_event_callback(channel, callback)
         data = client.recv(size)
+
         if data:
             tabela = data
-			# tabela is the data received from mobile application
-            print ("data received")
-            print (tabela)
-			
-		   # Here we send data to mobile application
-           # client.send(data)
+            if tabela == "PumpON":
+                print "Turning the water pump ON"
+                print "Please wait to read the next values from sensors"
+            if tabela == "PumpOFF":
+                print "Turning the water pump OFF"
+                print "Please wait to read the next values from sensors"
+            if tabela == "LedON":
+                print "Turning the cooler ON"
+                print "Please wait to read the next values from sensors"
+            if tabela == "LedOFF":
+                print "Turning the cooler OFF"
+                print "Please wait to read the next values from sensors"
+        humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+
 except:
     print("closing socket")
     client.close()
